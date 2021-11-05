b8c2802a4bfb18468fc29e857aea9fae93d28872
Bernard Jiang <hongyujiang@hotmail.com>
2021-11-05 01:54:02 +0000

    PerC=False, from True. batchsize to 128 from 200.

--- scripts/adaquant.sh
+++ scripts/adaquant.sh
@@ -1,4 +1,4 @@
-export datasets_dir=/media/drive/Datasets
+export datasets_dir=/workspace/develop/dataset
 export model=${1:-"resnet"}
 export model_vis=${2:-"resnet50"}
 export nbits_weight=${3:-4}
@@ -8,18 +8,24 @@ if [ "$5" = True ]; then
     export adaquant_suffix='.adaquant'
 fi
 export workdir=${model_vis}_w$nbits_weight'a'$nbits_act$adaquant_suffix
-export perC=True
+export perC=False 
 export num_sp_layers=-1
 export perC_suffix=''
 if [ "$perC" = True ] ; then
 export perC_suffix='_perC'
 fi
 # download and absorb_bn resnet50 and
+echo $model
+echo $workdir
+echo $model_vis
 python main.py --model $model --save $workdir -b 128  -lfv $model_vis --model-config "{'batch_norm': False}"
 
 # measure range and zero point on calibset
-python main.py --model $model  --nbits_weight $nbits_weight --nbits_act $nbits_act --num-sp-layers $num_sp_layers --evaluate results/$workdir/$model.absorb_bn --model-config "{'batch_norm': False,'measure': True, 'perC': $perC}" -b 100 --rec --dataset imagenet_calib --datasets-dir $datasets_dir
+python main.py --model $model  --nbits_weight $nbits_weight --nbits_act $nbits_act --num-sp-layers $num_sp_layers --evaluate results/$workdir/$model.absorb_bn --model-config "{'batch_norm': False,'measure': True, 'perC': $perC}" -b 128 --rec --dataset imagenet_calib --datasets-dir $datasets_dir
+
+
 if [ "$5" = True ]; then
 # Run adaquant to minimize MSE of the output with respect to range, zero point and small perturations in parameters
-python main.py --optimize-weights  --nbits_weight $nbits_weight --nbits_act $nbits_act  --num-sp-layers $num_sp_layers  --model $model -b 200 --evaluate results/$workdir/$model.absorb_bn.measure$perC_suffix --model-config "{'batch_norm': False,'measure': False, 'perC': $perC}" --dataset imagenet_calib --datasets-dir $datasets_dir --adaquant
+python main.py --optimize-weights  --nbits_weight $nbits_weight --nbits_act $nbits_act  --num-sp-layers $num_sp_layers  --model $model -b 128 --evaluate results/$workdir/$model.absorb_bn.measure$perC_suffix --model-config "{'batch_norm': False,'measure': False, 'perC': $perC}" --dataset imagenet_calib --datasets-dir $datasets_dir --adaquant
 fi
+
