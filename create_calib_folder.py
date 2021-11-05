908f4dc9850f2063da8620490a3e81b0bb1861bd
Bernard Jiang <hongyujiang@hotmail.com>
2021-11-05 01:54:33 +0000

    Changed for local run.

--- create_calib_folder.py
+++ create_calib_folder.py
@@ -2,18 +2,22 @@
 import os
 import shutil
 
-basepath = '/home/Datasets/imagenet/train/'
-basepath_calib = '/home/Datasets/imagenet/calib/'
+# basepath = '/home/bernard/develop/dataset/imagenet/train/'
+basepath = '/workspace/develop/dataset/imagenet_1k/train/'
+basepath_calib = '/workspace/develop/dataset/imagenet/calib/'
 
 directory = os.fsencode(basepath)
-os.mkdir(basepath_calib)
+if not os.path.exists(basepath_calib):
+    os.mkdir(basepath_calib)
 for d in os.listdir(directory):
     dir_name = os.fsdecode(d)
     dir_path = os.path.join(basepath,dir_name)
     dir_copy_path = os.path.join(basepath_calib,dir_name)
-    os.mkdir(dir_copy_path)
+    if not os.path.exists(dir_copy_path):
+        os.mkdir(dir_copy_path)
     for f in os.listdir(dir_path):
         file_path = os.path.join(dir_path,f)
         copy_file_path = os.path.join(dir_copy_path,f)
-        shutil.copyfile(file_path, copy_file_path)
+        # shutil.copyfile(file_path, copy_file_path)
+        os.symlink(file_path, copy_file_path)
         break
