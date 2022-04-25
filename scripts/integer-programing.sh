e86779faaaf535a7de491b71f2c4ac959394cbde
Bernard Jiang <hongyujiang@hotmail.com>
2022-04-25 12:01:46 -0700

    minor changes.

--- scripts/integer-programing.sh
+++ scripts/integer-programing.sh
@@ -30,7 +30,9 @@ export loss=${8:-'loss'}
 export layer_by_layer=results/$workdir_m2/$model.absorb_bn.measure$perC_suffix$adaquant_suffix.per_layer_accuracy.A$nbits_weight_m1.W$nbits_weight_m1.csv
 
 #Extract per layer loss delta
+echo "step 4"
 python main.py --model $model --evaluate results/$workdir_m2/$model.absorb_bn'.measure'$perC_suffix$adaquant_suffix --model-config "{'batch_norm': False,'measure': False, 'perC': $perC, 'depth': $depth}" -b 100 --dataset imagenet_calib --datasets-dir $datasets_dir --int8_opt_model_path results/$workdir_m2/$model.absorb_bn'.measure'$perC_suffix$adaquant_suffix --int4_opt_model_path results/$workdir_m1/$model.absorb_bn'.measure'$perC_suffix$adaquant_suffix --names-sp-layers '' --per-layer
 
 #Run IP algorithm to obtain best topology
+echo "step 5"
 python mpip_compression_pytorch_multi.py --model $model  --model_vis $model_vis --ip_method $loss --precisions $precisions --layer_by_layer_files $layer_by_layer --min_compression $min_compression --max_compression $max_compression $do_not_use_adaquant --datasets-dir $datasets_dir
