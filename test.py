3ca640e2a8995d649403f99a7741f7d4255cbb4c
Bernard Jiang <hongyujiang@hotmail.com>
2022-05-02 23:53:55 +0000

    updated.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import torch
 from torch.autograd.function import InplaceFunction, Function
+
 class Clamp(InplaceFunction):
     def forward(ctx, input,inplace, min, max):
         ctx.inplace = inplace                                                                          
@@ -16,18 +17,22 @@ class Clamp(InplaceFunction):
         grad_input = grad_output
         return grad_input,None, None, None
 
-a = torch.tensor([-2., -1., 0., 1., 2])
+q = torch.tensor(1.)
+q.requires_grad = True
+a = torch.tensor([-2000., -2., -1., 0., 1., -2., 100.])
 a.requires_grad = True
-b = torch.clamp(a, -1., 1.)
+b = torch.clamp(a, -q, q)
 b.retain_grad()
 c = b.mean()
 c.backward()
-print("Internal   Clamp(): ", b.grad, a.grad)
+print("Internal   Clamp(): ", c, b.grad, a.grad, q.grad)
 
-d = torch.tensor([-2., -1., 0., 1., 2])
+q2 = torch.tensor(1.)
+q2.requires_grad = True
+d = torch.tensor([-2000., -2., -1., 0., 1., -2., 100.])
 d.requires_grad = True
-e = Clamp().apply(d, False, -1., 1.)
+e = Clamp().apply(d, False, -q2, q2)
 e.retain_grad()
 f = e.mean()
 f.backward()
-print("Customized Clamp(): ", e.grad, d.grad)
+print("Customized Clamp(): ", f, e.grad, d.grad, q2.grad)
