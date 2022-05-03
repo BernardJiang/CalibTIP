9451412bd456b1a366f0a02dddc1aab6c215f391
Bernard Jiang <hongyujiang@hotmail.com>
2022-05-03 22:39:00 +0000

    Copied the proper json files.

--- scripts/advanced_pipeline_mobilenet.sh
+++ scripts/advanced_pipeline_mobilenet.sh
@@ -1,3 +1,11 @@
+
+cp modeljson/mobilenetv2/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json.4bit.0.999 results/mobilenet_v2_w4a8.adaquant/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json
+cp modeljson/mobilenetv2/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json.8bit.0.999 results/mobilenet_v2_w8a8.adaquant/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json
+
+# cp modeljson/mobilenetv2/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json.4bit.1 results/mobilenet_v2_w4a8.adaquant/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json
+# cp modeljson/mobilenetv2/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json.8bit.1 results/mobilenet_v2_w8a8.adaquant/mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json
+
+
 # rm results
 # ln -sf ../results.backup/results.20211221.parallel results
 sh scripts/adaquant.sh mobilenet_v2 mobilenet_v2 4 8 True mobilenetv2_adaquant.piano.kdp530.scaled.onnx.json
