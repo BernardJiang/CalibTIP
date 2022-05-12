1bd2d557da80b87e1b8d7161af34b9901eca1609
Bernard Jiang <hongyujiang@hotmail.com>
2022-05-11 18:17:27 -0700

    Weight + free scale of input and output. max_scale=1.0

--- scripts/advance2.sh
+++ scripts/advance2.sh
@@ -1,4 +1,4 @@
-# sh scripts/switchmax_scale.sh 1
-sh scripts/switchmax_scale.sh 0.999
+sh scripts/switchmax_scale.sh 1
+# sh scripts/switchmax_scale.sh 0.999
 sh scripts/advanced_pipeline_resnet.sh > logresnet.txt 2>logerrresnet.txt 
 sh scripts/advanced_pipeline_mobilenet.sh > logmobile.txt 2>logerrmobile.txt 
